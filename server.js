1d40ae7bb2dd5d4d725bfd02be353dfff1456a3a
Kasey Hinton <kaseyjameshinton@gmail.com>
2022-06-02 19:17:17 -0600

    feat: move to piku server

---
+++ server.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const express = require("express");
+const app = express(); // create express app
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
+
+// start express server on port 5000
+var port = process.env["PORT"] || 8000;
+app.listen(port, () => {
+  console.log("server started on port", port);
+});
